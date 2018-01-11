678a171875d5f24864125daf732182f2fc213720
412C\johangyu <whgksrb19@gmail.com>
2018-01-11 17:34:32 +0900

    iceServers stun , turn server mapping

--- dist/RTCMultiConnectionNew.js
+++ dist/RTCMultiConnectionNew.js
@@ -3174,8 +3174,11 @@ window.RTCMultiConnection = function(roomid, forceOptions) {
         function getIceServers(connection) {
             var iceServers = [];
 
+            iceServers.push(getSTUNObj('stun:stun.l.google.com:19302'));
+
             iceServers.push(getTURNObj('stun:14.41.55.87:3478','skyadmin','skyadmin'));
             iceServers.push(getTURNObj('turn:14.41.55.87:3478','skyadmin','skyadmin'));
+            iceServers.push(getTURNObj('turn:14.41.55.87:3478?transport=tcp','skyadmin','skyadmin'));
 
             // iceServers.push(getSTUNObj('stun:stun.l.google.com:19302'));
             //
